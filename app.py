53bcb81293c8649a9dbf084a30e153d05d383bbe
“Samuel <gathogosammy8@gmail.com>
2024-11-13 17:06:39 +0300

    make it better

--- app.py
+++ app.py
@@ -114,7 +114,8 @@ def fert_recommend():
     P = int(request.form['phosphorous'])
     K = int(request.form['pottasium'])
 
-    df = pd.read_csv('app\data\fertilizer.csv')
+    df = pd.read_csv(r'app\data\fertilizer.csv')
+
 
     nr = df[df['Crop'] == crop_name]['N'].iloc[0]
     pr = df[df['Crop'] == crop_name]['P'].iloc[0]
