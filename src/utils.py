a423c1b40dbdf3f1e19f4b3ebc869dae880be23b
samuel gathogo <111558517+samuelMG1@users.noreply.github.com>
2026-04-21 09:35:04 +0300

    Create src/utils.py with ModelUtils, DataUtils, and ValidationUtils classes

---
+++ src/utils.py
@@ -0,0 +1,17 @@
+class ModelUtils:
+    def __init__(self):
+        pass
+
+    # Add methods related to model utilities here
+
+class DataUtils:
+    def __init__(self):
+        pass
+
+    # Add methods related to data utilities here
+
+class ValidationUtils:
+    def __init__(self):
+        pass
+
+    # Add methods related to validation utilities here
